7097c52105b046c3b3201c9eea6cf32df3021aaa
unknown <henrytesting8@gmail.com>
2023-03-06 07:11:53 +0700

    menambahkan outlet dan children l-283

--- src/App.js
+++ src/App.js
@@ -17,6 +17,8 @@
 //DONE
 
 // 3. Add a root layout that adds the <MainNavigation> component above all page components
+//DONE
+
 // 4. Add properly working links to the MainNavigation
 // 5. Ensure that the links in MainNavigation receive an "active" class when active
 // 6. Output a list of dummy events to the EventsPage
@@ -24,25 +26,30 @@
 // 7. Output the ID of the selected event on the EventDetailPage
 // BONUS: Add another (nested) layout route that adds the <EventNavigation> component above all /events... page components
 
-import {createBrowserRouter,RouterProvider} from 'react-router-dom'
-import EditEventPage from './pages/EditEvent';
-import EventDetailPage from './pages/EventDetail';
-import EventsPage from './pages/Events';
-import HomePage from './pages/Home';
-import NewEventPage from './pages/NewEvent';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import EditEventPage from "./pages/EditEvent";
+import EventDetailPage from "./pages/EventDetail";
+import EventsPage from "./pages/Events";
+import HomePage from "./pages/Home";
+import NewEventPage from "./pages/NewEvent";
+import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
-  {path:'/', element:<HomePage/>},
-  {path:'/events', element:<EventsPage/>},
-  {path:'/events/:eventId', element:<EventDetailPage/>},
-  {path:'/events/new', element:<NewEventPage/>},
-  {path:'/events/:eventId/edit', element:<EditEventPage/>},
-
-])
-
+  {
+    path: "/",
+    element: <RootLayout />,
+    children: [
+      { index:true, element: <HomePage /> },
+      { path: "events", element: <EventsPage /> },
+      { path: "events/:eventId", element: <EventDetailPage /> },
+      { path: "events/new", element: <NewEventPage /> },
+      { path: "events/:eventId/edit", element: <EditEventPage /> },
+    ],
+  },
+]);
 
 function App() {
-  return <RouterProvider router={router}/>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
