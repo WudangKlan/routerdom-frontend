b9c8579603458fdcabef698cc9a36ca2a2019504
unknown <henrytesting8@gmail.com>
2023-03-11 08:49:09 +0700

    destructuring loader more leaner l-287

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,43 +1,22 @@
 // import { useEffect, useState } from 'react';
-import { useLoaderData } from 'react-router-dom';
-import EventsList from '../components/EventsList';
+import { useLoaderData } from "react-router-dom";
+import EventsList from "../components/EventsList";
 
 function EventsPage() {
   const events = useLoaderData();
 
-  // const [isLoading, setIsLoading] = useState(false);
-  // const [fetchedEvents, setFetchedEvents] = useState();
-  // const [error, setError] = useState();
-    
-  // useEffect(() => {
-  //   async function fetchEvents() {
-  //     setIsLoading(true);
-  //     const response = await fetch('http://localhost:8080/events');
-
-  //     if (!response.ok) {
-  //       setError('Fetching events failed.');
-  //     } else {
-  //       const resData = await response.json();
-  //       console.log(resData)
-  //       setFetchedEvents(resData.events);
-  //     }
-  //     setIsLoading(false);
-  //   }
-
-  //   fetchEvents();
-  // }, []);
-
-
-  return (
-    // <>
-    //   <div style={{ textAlign: 'center' }}>
-    //     {isLoading && <p>Loading...</p>}
-    //     {error && <p>{error}</p>}
-    //   </div>
-    //   {!isLoading && fetchedEvents && <EventsList events={fetchedEvents} />}
-    // </>
-    <EventsList events={events} />
-  );
+  return <EventsList events={events} />;
 }
 
 export default EventsPage;
+
+export async function loader() {
+  const response = await fetch("http://localhost:8080/events");
+
+  if (!response.ok) {
+    //
+  } else {
+    const resData = await response.json();
+    return resData.events;
+  }
+}
