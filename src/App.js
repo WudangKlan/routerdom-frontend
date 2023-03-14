f279b4b0bf53454aa65a31b1b5b00deccdf75604
unknown <henrytesting8@gmail.com>
2023-03-14 22:55:01 +0700

    working with action l-298

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ import EventDetailPage, {
 import EventsPage, { loader as eventsLoader } from "./pages/Events";
 import EventsRootLayout from "./pages/EventsRoot";
 import HomePage from "./pages/Home";
-import NewEventPage from "./pages/NewEvent";
+import NewEventPage, { action as newEventAction } from "./pages/NewEvent";
 import RootLayout from "./pages/Root";
 
 const router = createBrowserRouter([
@@ -63,7 +63,7 @@ const router = createBrowserRouter([
           },
           {
             path: ":eventId",
-            id:"event-detail",
+            id: "event-detail",
             loader: eventDetailLoader,
             children: [
               {
@@ -73,7 +73,7 @@ const router = createBrowserRouter([
               { path: "edit", element: <EditEventPage /> },
             ],
           },
-          { path: "new", element: <NewEventPage /> },
+          { path: "new", element: <NewEventPage />, action: newEventAction },
         ],
       },
     ],
